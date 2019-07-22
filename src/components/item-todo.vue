a5ee73e16d66dd0648ace58c4a54ec807b0733ac
luhaojian11 <962671114@qq.com>
2019-07-22 22:13:37 +0800

    [Allard] 鼠标双击编辑修改Todo Item， 按回车确认修改

--- src/components/item-todo.vue
+++ src/components/item-todo.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <li><input type="checkbox" :checked="toDo.status" v-model="toDo.status"  >
-      <span :class="{ checked: toDo.status }">
+    <li><input type="checkbox" :checked="toDo.status" v-model="toDo.status">
+      <span :class="{ checked: toDo.status }"
+            :contenteditable="editable"
+            @dblclick="setEditable(true)"
+            @keypress.enter="onEnterPress"
+            ref="text">
         {{toDo.information}}
       </span>
     </li>
@@ -14,6 +18,21 @@
     props: {
       toDo: Object
     },
+    data() {
+      return {
+        editable: false
+      }
+    },
+    methods: {
+      setEditable(editable) {
+        this.editable = editable;
+      },
+      onEnterPress() {
+        this.setEditable(false);
+        this.toDo.information = this.$refs.text.innerText;
+
+      }
+    }
   }
 </script>
 
