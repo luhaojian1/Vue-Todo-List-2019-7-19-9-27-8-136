fb2f1ab759aff1e6269ecaab87489f82421668db
luhaojian11 <962671114@qq.com>
2019-07-22 20:16:33 +0800

    update error

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <ToDoList></ToDoList>
+  </div>
+</template>
+
+<script>
+  import ToDoList from "./components/TodoList"
+export default {
+  name: 'App',
+  components:{
+    ToDoList
+  }
+}
+</script>
+
+<style>
+
+</style>
