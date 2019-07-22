1e70c31f0e191995dc9e7e7a8c3e56710ea05fdd
luhaojian1 <962671114@qq.com>
2019-07-22 18:12:39 +0800

    format code

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app" class="container">
+    <Header></Header>
+   <ListCreation></ListCreation>
+  </div>
+</template>
+
+<script>
+  import Header from "./components/Header"
+  import ListCreation from "./components/ListCreation"
+export default {
+  name: 'App',
+  components:{
+    ListCreation,
+    Header
+  }
+}
+</script>
+
+<style>
+
+</style>
