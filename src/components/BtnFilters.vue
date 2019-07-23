214d4fe46405c72de9a9af502d802e7081cdec5b
luhaojian1 <962671114@qq.com>
2019-07-23 12:30:04 +0800

    提取，细化组件

---
+++ src/components/BtnFilters.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="filters">
+    <div class="filter">
+      <button v-for="(filter,i) in filters"
+              :key="i" :value="filter"
+              :filter="filter"
+              :class="['btnFilter', { selected: choiceBtn === filter }]"
+              @click=" clickFilterButton(filter)">{{filter}}
+      </button>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: "BtnFilters",
+    data() {
+      return {
+        choiceBtn: this.$store.state.choiceBtn,
+        filters: ['All', 'Active', 'Complete']
+      }
+    },
+    methods: {
+      clickFilterButton(filter) {
+        this.$store.commit('updateChoiceBtn', filter);
+      },
+    },
+
+
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/todo_adder.vue
@@ -0,0 +1,36 @@
+<template>
+  <input class="input-text" id="inputInfo" type="text" v-model="toDoInfo">
+  <button id="button" v-on:click="addToDoItem()">Add</button>
+</template>
+
+<script>
+    export default {
+      name: "todo_adder",
+      data() {
+        return {
+          toDoInfo: '',
+          todoId: 0,
+
+        }
+      },
+      methods: {
+        addToDoItem() {
+          if (this.toDoInfo === '') {
+            alert("input can not null");
+            return;
+          }
+          let todo = {
+            information: this.toDoInfo,
+            status: false,
+            todoId: this.todoId++
+          };
+          this.$store.commit('addTodoItem', todo);
+          this.toDoInfo = '';
+        },
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
